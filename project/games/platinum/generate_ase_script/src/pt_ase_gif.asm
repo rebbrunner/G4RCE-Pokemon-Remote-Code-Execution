5dd188d886ee1e44d91f131c4c27d53c508bff2c
Rebecca Brunner <brunner@bgsu.edu>
2025-01-11 22:00:31 -0500

    Working improved bootstrap

--- project/games/platinum/generate_ase_script/src/pt_ase_gif.asm
+++ project/games/platinum/generate_ase_script/src/pt_ase_gif.asm
@@ -7,41 +7,30 @@
 _start:
 push {r1-r7, lr}
 
-add r3, #0x10
-ldmia r3, {r1, r2, r4, r5}
-@ r1 = base address
-@ r2 = end script
-@ r4 = gift id offset
-@ r5 = dot artist app enabled offset
+mov r2, #0x1F
+ldrh r1, [r3, r2] @ r1 = diff gift id offset & dot artist offset 0xA44C
 
-@ get base
-ldr r1,[r1]
+sub r3, #0x15
+mov r2, #0x6
+strb r2, [r3]
 
-@ reset gift ID
-mov r6,#0x6
-str r6, [r1, r4]
-
-@ enable dots artist if needed
-ldr r6, [r1, r5]
-cmp r6, #0x1
-beq _return
-
-mov r6,#0x1
-str r6,[r1,r5]
-
-@ end script (no jump to dot artist)
-blx r2
+@ enable dots artist
+sub r1, r3, r1
+mov r2, #0x1
+strb r2, [r1]
 
 @ set ASE return point to dots artist
-add r6, #0x16
-str r0, [r6]
+add r1, #0x14
+str r1, [r0, #0x8]
+
+@ write end to padding between dots artist and calendar app
+mov r2, #0x2
+add r1, #0x69
+strb r2, [r1]
 
 _return:
 pop {r1-r7, pc}
 
 _data:
-nop
-.word 0x02101D40                        @ base pointer
-.word 0x0203E76D                        @ end script mode function
-.word 0xB5D4                           @ gift ID offset (assuming first gift slot)
-.word 0x1188                           @ dots artist enabled boolean
+@ .word 0x0203E76D                        @ end script mode function
+.word 0xA44E                              @ diff gift id offset & dot artist offset 0xA44C
