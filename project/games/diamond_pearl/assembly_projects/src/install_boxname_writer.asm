665626e72fd76dafebc88b402ec4b562ab971cbc
Rebecca Brunner <brunner@bgsu.edu>
2026-05-02 23:17:31 -0400

    debugging

--- project/games/diamond_pearl/assembly_projects/src/install_boxname_writer.asm
+++ project/games/diamond_pearl/assembly_projects/src/install_boxname_writer.asm
@@ -7,8 +7,8 @@
 src             .req r0
 dest            .req r1
 size            .req r2
-base            .req r3
 offset          .req r4
+base            .req r5
 
 _start:
 .code   16
@@ -16,7 +16,7 @@ _start:
 push {r0-r7,lr}
 
 @ read in base pointer, set src pointer to data location
-ldr src, = _data
+adr src, _data
 ldr base,[src]
 ldr base,[base]
 add src,#0x4
@@ -26,10 +26,12 @@ ldrh offset, [src]
 cmp offset, #0x0
 beq _end
 
-ldrh size,[src,#0x2]       @ r0 = src, r1 = dest, r2 = size -> copy data swi call
+ldrh size,[src,#0x2]       @ r0 = src, r1 = dest, r2 = size (halfwords) -> copy data swi call
 add dest,base,offset
 add src,src,#0x4
 swi 0xB
+mov r6,#0x2                @ halfwords -> bytes
+mul size,size,r6
 add src,src,size
 b _loop
 
@@ -38,4 +40,4 @@ pop {r0-r7,pc}
 
 .balign 4
 _data:
-.word 0x2111880             @ base address
+.word 0x02106FC0             @ base address
