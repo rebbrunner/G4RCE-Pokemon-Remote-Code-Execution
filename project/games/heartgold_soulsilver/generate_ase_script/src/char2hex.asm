ce801a63a569a7f929c71c059403e69c9487bc3f
Rebecca Brunner <brunner@bgsu.edu>
2026-07-09 21:58:25 -0400

    Made payload execution location more dynamic for better compatibility

--- project/games/heartgold_soulsilver/generate_ase_script/src/char2hex.asm
+++ project/games/heartgold_soulsilver/generate_ase_script/src/char2hex.asm
@@ -8,60 +8,58 @@ _start:
 push {r0-r7,lr}
 
 @ load base pointer
-add r3,#0xDC
+adr r3,_data
 ldr r0,[r3,#0x0]
 ldr r0,[r0]                     @ loaded base poniter
 
 @ Reset gift - o
-ldr r1,[r3,#0x4]                @ gift location
+ldr r1,_gift_offset                @ gift location
 mov r2,#0x6
 strb r2,[r0,r1]
 
 @ Setup NPC ASE - o
-ldr r1,[r3,#0x8]                @ NPC location
-ldrh r2,[r3,#0xC]
+ldr r1,_npc_script_offset                @ NPC location
+ldrh r2,_nickname_offset
 strh r2,[r0,r1]
 
 @ Check OP mode - o
-ldr r1,[r3,#0x10]
-ldrb r2,[r0,r1]
+ldr r1,_box_name_offset
+add r1,r0,r1
+ldrb r2,[r1]
 cmp r2,#0x2B
 bne _WRITER
 
 @ ASE mod - o
-ldr r1,[r3,#0x14]               @ base -> execution spot (-)
-ldr r2,[r3,#0x18]               @ base -> ASE return point
+ldr r1,_sig_offset
 add r1,r0,r1
-str r1,[r0,r2]
-b _end
+pop {r0}
+str r1,[r0,#0x8]
+pop {r1-r7,pc}
 
 _WRITER:
-ldr r4,[r3,#0x1C]
+ldr r4,_const_char_encoding_val
 add r1,r1,#0x2
 bl _decode
 mov r7,r5
 add r1,r1,#0x4
 bl _decode
-ldr r2,[r3,#0x14]                        
+ldr r2,_sig_offset                        
 add r2,r2,r0                    @ r2 = location; r7 = size;
 add r2,r2,r5
 add r1,r1,#0xA
 
 _write:
 cmp r7,#0x0                     @ if size 0; end
-beq _p_end
+beq _end
 sub r7,r7,#0x1                  @ decrement loop counter (size)
 
 add r1,r1,#0x18
-mov r5,#0x5
-push {r5}
+mov r3,#0x5
 
 _mini_loop:
-pop {r5}
-sub r5,r5,#0x1
-cmp r5,#0x0
+sub r3,r3,#0x1
+cmp r3,#0x0
 beq _write
-push {r5}
 
 bl _decode                      @ decode r4 command into r5 (destroys data in r6 as well)
 strb r5,[r2]                      @ store value of r5 in location r2
@@ -78,22 +76,23 @@ lsl r5,r5,#0x4
 add r5,r5,r6
 bx lr
 
-_p_end:
-pop {r2}
-b _end
-
 _end:
 pop {r0-r7,pc}
 
+.balign 4
 _data:
-nop                             @ odd needs nop, even no nop
 .word 0x2111880                 @ base pointer
+_gift_offset:
 .word 0x9E4C                    @ base -> gift offset
+_npc_script_offset:
 .word 0x26250                   @ base -> NPC script offset
+_nickname_offset:
 .word 0x9C9C                    @ NPC script offset -> nickname
+_box_name_offset:
 .word 0x21718                   @ base -> box name
+_sig_offset:
 .word 0x4548                    @ base -> execution spot (-)
-.word 0x2AA50                   @ base -> ASE return point
+_const_char_encoding_val:
 .word 0x121                     @ character encoding value
 
 @ W0403 02 01
